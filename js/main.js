4f2cb98b5efb1cf2a0cc5eb31689307b1290cc84
Winchestor <75200506+Winchestor2001@users.noreply.github.com>
2021-09-04 00:11:06 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,71 @@
 $(function(){
 
 
-    $('.content__box').click(function(event){
-        let boxText = $('.content__box-title').text()
-        let data = $(event.target).closest('content__box-title').find('h1')
+    const div = document.querySelector( ".content__inner" ); // находим первый элемент <div> в документе
+	div.addEventListener("click", showTargets); // регистрируем обработчик события "click" для элемента <div>
+
+	function showTargets( event ) {
+        let res = event.target.innerText
         
-    });
+        if (res == 'Пробив по номеру'){
+            $('.modal_1').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'МВД'){
+            $('.modal_2').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'ФНС'){
+            $('.modal_3').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'ПФР'){
+            $('.modal_4').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'Мини-досье (автовыдача)'){
+            $('.modal_5').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'Верефикация Готовые кошельки'){
+            $('.modal_6').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'Мобильные операторы'){
+            $('.modal_7').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'Флуд и рассылка'){
+            $('.modal_8').css({opacity: '1', visibility: 'visible'});
+        }
+        else if (res == 'Пробив КИ'){
+            $('.modal_9').css({opacity: '1', visibility: 'visible'});
+        }
+	}
 
 
     $('.hamburger').click(function(){
         $('.menu').toggleClass('active')
     });
+
+
+    $('.cencel').click(function(){
+        $('.modal_1').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_2').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_3').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_4').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_5').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_6').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_7').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_8').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_9').css({opacity: '0', visibility: 'hidden'});
+    });
+    $('.modal__btn').click(function(){
+        $('.modal_1').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_2').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_3').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_4').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_5').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_6').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_7').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_8').css({opacity: '0', visibility: 'hidden'});
+        $('.modal_9').css({opacity: '0', visibility: 'hidden'});
+    });
+
+
+
 
 });
